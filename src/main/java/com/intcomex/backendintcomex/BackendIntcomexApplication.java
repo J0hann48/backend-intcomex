bb6dd81a8c33d1f4d57465fffbd410a9a6b6f78d
Johann Torres <joahnn.1996@gmail.com>
2024-06-10 10:57:45 -0500

    First commit}

---
+++ src/main/java/com/intcomex/backendintcomex/BackendIntcomexApplication.java
@@ -0,0 +1,13 @@
+package com.intcomex.backendintcomex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIntcomexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIntcomexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intcomex/backendintcomex/BackendIntcomexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intcomex.backendintcomex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIntcomexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
